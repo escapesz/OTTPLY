9a2d23e557035b4694defff0155d04c5e9c2902e
escapesz <61728173+escapesz@users.noreply.github.com>
2026-09-04 20:31:51 +0800

    Update combine.py

--- combine.py
+++ combine.py
@@ -1,9 +1,11 @@
 import urllib.request
 
-EXTERNAL_M3U = "https://raw.githubusercontent.com/ryansnetcafe/ott-playlist/refs/heads/main/ryansnetcafe.m3u"
+EXTERNAL_M3U_1 = "https://raw.githubusercontent.com/ryansnetcafe/ott-playlist/refs/heads/main/ryansnetcafe.m3u"
+EXTERNAL_M3U_2 = "https://cdn.djdoolky76.net/udptv/phc-free.m3u"
 
 LOCAL_M3U = "my_channels.m3u"
 OUTPUT_M3U = "combined.m3u"
+
 
 def download(url):
     req = urllib.request.Request(
@@ -13,13 +15,27 @@ def download(url):
     with urllib.request.urlopen(req, timeout=30) as response:
         return response.read().decode("utf-8", errors="ignore")
 
+
+def clean_playlist(playlist):
+    return playlist.replace("#EXTM3U", "", 1).strip()
+
+
 local = open(LOCAL_M3U, "r", encoding="utf-8").read()
-external = download(EXTERNAL_M3U)
 
-# Remove #EXTM3U from external playlist
-external = external.replace("#EXTM3U", "", 1).strip()
+external_1 = download(EXTERNAL_M3U_1)
+external_2 = download(EXTERNAL_M3U_2)
 
-combined = local.rstrip() + "\n" + external + "\n"
+external_1 = clean_playlist(external_1)
+external_2 = clean_playlist(external_2)
+
+combined = (
+    local.rstrip()
+    + "\n"
+    + external_1
+    + "\n"
+    + external_2
+    + "\n"
+)
 
 open(OUTPUT_M3U, "w", encoding="utf-8").write(combined)
 
